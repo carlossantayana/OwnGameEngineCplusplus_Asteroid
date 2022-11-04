3612c7681025deca2780216dcc08d15508b88516
Sebastian02L <sebaslf02@gmail.com>
2022-11-04 17:13:58 +0100

    Teapot Hereda de Solid

--- P1_HolaMundo/Teapot.cpp
+++ P1_HolaMundo/Teapot.cpp
@@ -2,12 +2,12 @@
 
 void Teapot::Render() {
 	glPushMatrix();
-	glTranslatef(this->GetPos().GetCoordinateX(), this->GetPos().GetCoordinateY(), this->GetPos().GetCoordinateZ());
-	glColor3f(this->GetColor().GetRedComponent(), this->GetColor().GetGreenComponent(), this->GetColor().GetBlueComponent());
+	glTranslatef(this->GetVectorCoordinate().GetCoordinateX(), this->GetVectorCoordinate().GetCoordinateY(), this->GetVectorCoordinate().GetCoordinateZ());
+	glColor3f(this->GetVectorColor().GetRedComponent(), this->GetVectorColor().GetGreenComponent(), this->GetVectorColor().GetBlueComponent());
 
-	glRotatef(this->GetOrientation().GetCoordinateX(), 1.0, 0.0, 0.0);
-	glRotatef(this->GetOrientation().GetCoordinateY(), 0.0, 1.0, 0.0);
-	glRotatef(this->GetOrientation().GetCoordinateZ(), 0.0, 0.0, 1.0);
+	glRotatef(this->GetVectorOrientation().GetCoordinateX(), 1.0, 0.0, 0.0);
+	glRotatef(this->GetVectorOrientation().GetCoordinateY(), 0.0, 1.0, 0.0);
+	glRotatef(this->GetVectorOrientation().GetCoordinateZ(), 0.0, 0.0, 1.0);
 
 	glutSolidTeapot(this->GetSize());
 	glPopMatrix();
